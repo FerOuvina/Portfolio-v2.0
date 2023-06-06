36d33c1314257adfd7b3da02b07b749d93f0ce62
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-06 18:57:06 -0300

    Adapting the main page to render on client bc of the useEffect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
+'use client';
+
+import Content from '@/components/content';
+import Navbar from '@/components/navbar';
+import '../stylesheets/page.css';
+
 export default function Home() {
   return (
-    <div>Waos</div>
-  )
+    <div className='mainContainer'>
+      <Navbar />
+      <Content />
+    </div>
+  );
 }
