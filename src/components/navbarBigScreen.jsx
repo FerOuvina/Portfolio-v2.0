404e4041d489a7691cb1f408a98a9d642f01d4d7
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-06 18:57:39 -0300

    Creating navbar component for small screens

---
+++ src/components/navbarBigScreen.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import homeIcon from '../assets/home.png';
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <header className='fixed bottom-0 z-10 p-2 w-full backdrop-blur-sm bg-darkBrown'>
+      <nav className='container flex items-center'>
+        <ul className='flex items-center w-full'>
+          <Link legacyBehavior href={'#'}>
+            <a>
+              <li className='mr-4'>{'Home'}</li>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href={'#'}>
+            <a>
+              <li className='mr-4'>{'About'}</li>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href={'#'}>
+            <a>
+              <li className='mr-4'>{'Projects'}</li>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href={'#'}>
+            <a>
+              <li className='mr-4'>{'Contact'}</li>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href={'#'}>
+            <a>
+              <li className='mr-4'>{'Blog'}</li>
+            </a>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
