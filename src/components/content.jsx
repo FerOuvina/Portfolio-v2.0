7bca0bfbc67b8850c5fa260a5c8f06fc9a9980d9
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-07 12:15:50 -0300

    Creating welcome card component and adding it into the main content

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,24 +1,10 @@
-import Image from 'next/image';
-import blueCircle from '../assets/blue-circle.png';
-import brownCircle from '../assets/brown-circle.png';
-import '../stylesheets/content.css';
+import WelcomeCard from './welcomeCard';
 
 export default function Content() {
   return (
     <main className='flex flex-col justify-center items-center h-screen'>
-      <div className='bg-white folderContainer'>
-        <div className='flex justify-between border-b-2 border-black'>
-          <h4 className='pl-1 text-brown'>/home/welcome</h4>
-          <div className='flex items-center'>
-            <Image src={blueCircle} alt='blue-circle' width={20} height={20} />
-            <Image src={brownCircle} alt='blue-circle' width={20} height={20} />
-          </div>
-        </div>
-        <div className='container p-5 pb-40 w-max text-white bg-brown'>
-          <h1 className=''>Fernando Ouviña</h1>
-          <h2 className=''>Front End Developer</h2>
-        </div>
-      </div>
+      {/* Presentation Card */}
+      <WelcomeCard />
     </main>
   );
 }
---
+++ src/components/welcomeCard.jsx
@@ -0,0 +1,45 @@
+import Card from './card';
+import Link from 'next/link';
+
+export default function WelcomeCard() {
+  return (
+    <Card
+      navTitle={'/home/welcome'}
+      contentTitle={'Welcome!'}
+      contentH1={`I'm Fernando Ouviña`}
+      contentH2={`I'm a Front End Developer`}
+    >
+      <footer className='container bg-white'>
+        <ul className='flex justify-around p-2 text-brown'>
+          <Link
+            legacyBehavior
+            href={'https://github.com/FerOuvina'}
+            referrerPolicy='no-referrer'
+          >
+            <a>
+              <li title='Github Profile'>#github</li>
+            </a>
+          </Link>
+          <Link
+            legacyBehavior
+            href={'https://www.linkedin.com/in/fernando07'}
+            referrerPolicy='no-referrer'
+          >
+            <a>
+              <li title='LinkedIn Profile'>#linkedin</li>
+            </a>
+          </Link>
+          <Link
+            legacyBehavior
+            href={'example.com'}
+            referrerPolicy='no-referrer'
+          >
+            <a>
+              <li title='Personal Blog'>#blog</li>
+            </a>
+          </Link>
+        </ul>
+      </footer>
+    </Card>
+  );
+}
