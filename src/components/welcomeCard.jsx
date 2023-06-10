b20a89b5e91bb64e2b7a5f6ef1b455601091b59c
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-10 18:16:16 -0300

    Adding logos and using mixed arrays to pass html on props

--- src/components/welcomeCard.jsx
+++ src/components/welcomeCard.jsx
@@ -1,15 +1,30 @@
 import Card from './card';
 import Link from 'next/link';
+import Image from 'next/image';
+import gitHubLogo from '../assets/GitHub-Logo.svg';
+import linkedInLogo from '../assets/LinkedIn-Logo.svg';
+import blogLogo from '../assets/Blog-Logo.svg';
 
 export default function WelcomeCard() {
   return (
     <Card
       navTitle={'/home/welcome'}
-      contentTitle={'Welcome!'}
+      contentFirst={'Welcome!'}
       contentH1={`I'm Fernando Ouviña`}
       contentH2={`I'm a Front End Developer`}
+      contentText={[
+        `Check out my github to see what I'm working on`,
+        <br key={'welcomeCard-br'} />,
+        `Also check my LinkedIn I `,
+        <em key={'welcomeCard-sometimes'}>(sometimes)</em>,
+        ` post about my projects there.`,
+        <br key={'welcomeCard-br2'} />,
+        `And don't forget to visit my blog too `,
+        <em key={'welcomeCard-wip'}>(still in progress),</em>,
+        ` I post some interesting stuff there.`,
+      ]}
       id={'welcomeCard'}
-      width={250}
+      width={280}
     >
       <footer className='container bg-white'>
         <ul className='flex justify-around p-2 text-brown'>
@@ -19,7 +34,15 @@ export default function WelcomeCard() {
             referrerPolicy='no-referrer'
           >
             <a>
-              <li title='Github Profile'>#github</li>
+              <li title='Github Profile'>
+                <Image
+                  src={gitHubLogo}
+                  alt='github logo'
+                  width={30}
+                  height={30}
+                  title='Github'
+                />
+              </li>
             </a>
           </Link>
           <Link
@@ -28,7 +51,15 @@ export default function WelcomeCard() {
             referrerPolicy='no-referrer'
           >
             <a>
-              <li title='LinkedIn Profile'>#linkedin</li>
+              <li title='LinkedIn Profile'>
+                <Image
+                  src={linkedInLogo}
+                  alt='linkedIn logo'
+                  width={30}
+                  height={30}
+                  title='LinkedIn'
+                />
+              </li>
             </a>
           </Link>
           <Link
@@ -37,7 +68,15 @@ export default function WelcomeCard() {
             referrerPolicy='no-referrer'
           >
             <a>
-              <li title='Personal Blog'>#blog</li>
+              <li title='Personal Blog'>
+                <Image
+                  src={blogLogo}
+                  alt='blog logo'
+                  width={30}
+                  height={30}
+                  title='Blog'
+                />
+              </li>
             </a>
           </Link>
         </ul>
