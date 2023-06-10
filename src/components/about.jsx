c3d5752b0cd26c6b1a2f468fdb92f34bde577745
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-10 18:14:39 -0300

    Creating a footer with the logos and using mixed arrays to add html trough props

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,118 @@
+import Image from 'next/image';
 import Card from './card';
+import javaScriptLogo from '../assets/JavaScript-Logo.svg';
+import reactLogo from '../assets/React-Logo.svg';
+import nextLogo from '../assets/Next-Logo.svg';
+import tailwindLogo from '../assets/Tailwind-Logo.svg';
+import gitLogo from '../assets/Git-Logo.svg';
+import javaLogo from '../assets/java-logo.svg';
+import CSSLogo from '../assets/CSS-Logo.svg';
+import HTMLLogo from '../assets/HTML-Logo.svg';
+import '../stylesheets/about.css';
 
 export default function About() {
+  const imgWidth = 35;
+  const imgHeight = 35;
+
   return (
     <Card
       navTitle={`/specifications/about`}
-      contentTitle={`I'm a passionate web developer who loves to create beautiful and functional websites, and applications. I'm currently looking for opportunities to grow my skills and a chance to start my career as a developer.`}
-      contentH1={`This is my current stack`}
+      contentFirst={'About Me'}
+      contentText={[
+        `I'm a passionate web developer who loves to create beautiful and functional websites, and applications.`,
+        <br key={'specificationsCard-br'} />,
+        `I'm currently looking for opportunities to grow my skills and a chance to start my career as a developer.`,
+      ]}
       id={`specificationsCard`}
       width={300}
     >
-      <footer></footer>
+      <footer className='w-full bg-white aboutContainer'>
+        <h2 className='text-lg text-center text-brown'>
+          This is my current stack
+        </h2>
+        <hr className='w-full border-2 breakLine' />
+        <ul className='grid grid-cols-4 gap-2 justify-items-center mx-2 my-3'>
+          <li>
+            <Image
+              src={javaScriptLogo}
+              alt='JavaScript Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='JavaScript'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={reactLogo}
+              alt='React Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='React'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={nextLogo}
+              alt='Next Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='Next.js'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={tailwindLogo}
+              alt='TailwindCSS Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='TailwindCSS'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={gitLogo}
+              alt='Git Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='Git'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={javaLogo}
+              alt='Java Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='Java'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={CSSLogo}
+              alt='CSS Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='CSS'
+            />
+          </li>
+
+          <li>
+            <Image
+              src={HTMLLogo}
+              alt='HTML Logo'
+              width={imgWidth}
+              height={imgHeight}
+              title='HTML'
+            />
+          </li>
+        </ul>
+      </footer>
     </Card>
   );
 }
