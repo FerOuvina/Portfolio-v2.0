c5035046c08c0c935ba94e1c221cba1650b99c1f
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-12 17:23:18 -0300

    Contact "NO" option, still needs changes

---
+++ src/components/contactNo.jsx
@@ -0,0 +1,25 @@
+import Card from './card';
+
+export default function ContactNo() {
+  return (
+    <Card
+      navTitle={'/contact/contact-failed'}
+      contentFirst={[
+        <div
+          key={`contactNoContainer`}
+          className='flex flex-col justify-center items-center'
+        >
+          <span>You choose wrong!</span>
+        </div>,
+      ]}
+      contentH1={[
+        `This device will self destruct now`,
+        <br key={'contactFailded-br'} />,
+        <br key={'contactFailded-br'} />,
+        `Unless you press the big red button below`,
+      ]}
+      id={'contactNo'}
+      width={300}
+    ></Card>
+  );
+}
