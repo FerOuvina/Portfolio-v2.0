ed9c1c740c2f10c2fe7725c36b3cbf6c0354e3d5
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-12 17:23:56 -0300

    Adding the remaining cards to the content

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,10 +1,33 @@
+import About from './about';
+import Blog from './blog';
+import Contact from './contact';
+import DesktopIcons from './desktopIcons';
+import Projects from './projects';
 import WelcomeCard from './welcomeCard';
 
 export default function Content() {
   return (
     <main className='flex flex-col justify-center items-center h-screen'>
-      {/* Presentation Card */}
-      <WelcomeCard />
+      <section className='fixed top-0'>
+        <DesktopIcons />
+      </section>
+
+      <section className='flex flex-col justify-center items-center'>
+        {/* Presentation Card */}
+        <WelcomeCard />
+
+        {/* About Card */}
+        <About />
+
+        {/* Projects Card */}
+        <Projects />
+
+        {/* Contact Card */}
+        <Contact />
+
+        {/* Blog Card */}
+        <Blog />
+      </section>
     </main>
   );
 }
