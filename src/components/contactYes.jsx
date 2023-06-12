e973a9ba164336872cf6798ce1cd1fc230692544
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-06-12 17:23:39 -0300

    Contact "YES" option, still needs changes

---
+++ src/components/contactYes.jsx
@@ -0,0 +1,65 @@
+import Card from './card';
+import '../stylesheets/contactYes.css';
+
+export default function ContactYes() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Card
+      navTitle={'/contact/contact-success'}
+      contentFirst={[
+        <div
+          key={`contactYesContainer`}
+          className='flex flex-col justify-center items-center bg-white'
+        >
+          <span>Awesome!</span>
+          <span>Now please fill this form</span>
+        </div>,
+      ]}
+      contentH1={'waos'}
+      id={'contactYes'}
+      width={300}
+    >
+      <div className='container bg-white text-darkBrown formContainer'>
+        <form className='flex flex-col gap-2 p-4' onSubmit={handleSubmit}>
+          <div className='flex p-1 w-full form-group bg-aquaGreen'>
+            <label htmlFor='name'>Name:</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className='w-full bg-aquaGreen'
+            />
+          </div>
+
+          <div className='flex p-1 w-full form-group bg-aquaGreen'>
+            <label htmlFor='email'>Email:</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='w-full bg-aquaGreen'
+            />
+          </div>
+
+          <div className='flex flex-col p-1 w-full form-group bg-aquaGreen'>
+            <label htmlFor='message'>Message</label>
+            <textarea
+              id='message'
+              name='message'
+              className='w-full bg-aquaGreen'
+            />
+          </div>
+
+          <div className='flex justify-center items-center w-full'>
+            <button type='submit' className='px-10 py-1 text-center text-brown'>
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </Card>
+  );
+}
