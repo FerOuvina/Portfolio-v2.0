1a4842c52491687ad2147ef8aba9204cbd5c98b3
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-07-14 11:54:32 -0300

    Finishing the interaction with the button

--- src/components/button-contactNo.jsx
+++ src/components/button-contactNo.jsx
@@ -1,8 +1,23 @@
+import { gsap } from 'gsap';
+import useSound from 'use-sound';
+import useObserver from '@/hooks/useObserver';
 import '../stylesheets/button-contactNo.css';
 
 export default function ButtonContactNo() {
+  const isIntersecting = useObserver('contactNo');
+  const [playAlert, { stop }] = useSound('/sounds/Alert.mp3', {
+    volume: 0.2,
+    playbackRate: 0.95,
+  });
+  isIntersecting ? playAlert() : stop();
+
   const handleClick = () => {
-    
+    gsap.to('#contactNo', {
+      duration: 0.3,
+      opacity: 0,
+      scale: 0,
+      display: 'none',
+    });
   };
 
   return (
