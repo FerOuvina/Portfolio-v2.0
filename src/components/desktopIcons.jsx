e53abfce1498f13ef225c7d4382fb87b693033d5
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-07-20 19:05:37 -0300

    Changing animation display

--- src/components/desktopIcons.jsx
+++ src/components/desktopIcons.jsx
@@ -52,7 +52,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: '0',
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
@@ -76,7 +76,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: 0,
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
@@ -100,7 +100,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: 0,
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
@@ -124,7 +124,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: 0,
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
@@ -148,7 +148,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: 0,
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
@@ -172,7 +172,7 @@ export default function DesktopIcons() {
       {
         duration: 0.4,
         x: 0,
-        y: 0,
+        y: -30,
         scale: 1.0,
         position: 'absolute',
         display: 'flex',
